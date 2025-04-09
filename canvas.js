cb3828ac0971a271f1462cb10206948283ad9355
Yash Kalani <yash@kalani.name>
2025-04-09 18:46:10 -0400

    week 1 and week 2
    created 3 features and made the simulation for the falling sand

--- canvas.js
+++ canvas.js
@@ -174,7 +174,7 @@ export function clearGrid() {
  */
 export function checkBounds(row, col) {
     // TODO make sure row and col are within the grid
-    return true;
+    return row >= 0 && row < grid.length && col >= 0 && col < grid[0].length;
 }
 
 /**
@@ -191,7 +191,26 @@ export function checkBounds(row, col) {
  * @returns {boolean} If the particle was moved or not
  */
 export function moveParticle(row, col, newRow, newCol, swap) {
-    // TODO move a particle from (row, col) to (newRow, newCol)
+    if (!checkBounds(row, col) || !checkBounds(newRow, newCol)) {
+        return false;
+    }
+
+    if (getParticle(newRow, newCol)) {
+        // If there is a particle but we can swap then flip the particles
+        if (swap && swap(getParticle(newRow, newCol))) {
+            const temp = grid[newRow][newCol];
+            grid[newRow][newCol] = grid[row][col];
+            grid[row][col] = temp;
+            return true;
+        }
+        // If we can't swap then don't move
+        else {
+            return false;
+        }
+    }
+
+    grid[newRow][newCol] = grid[row][col];
+    grid[row][col] = null;
     return true;
 }
 
@@ -205,7 +224,15 @@ export function redraw() {
     // Loop through all elements in the grid
     for (let row = 0; row < grid.length; row++) {
         for (let col = 0; col < grid[0].length; col++) {
-            // TODO draw particles to screen
+            const particle = grid[row][col];
+
+            // Check if there is a particle at (row, col). (null == false)
+            if (particle) {
+                // Get particle color
+                ctx.fillStyle = particle.color;
+                // Draw particle (multiple by eachSize to scale it from grid coordinates to pixels)
+                ctx.fillRect(col * eachSize, row * eachSize, eachSize, eachSize);
+            }
         }
     }
 }
--- particles.js
+++ particles.js
@@ -46,12 +46,25 @@ export class Sand extends Particle {
 
     swap(other) {
         // TODO make sand fall under the water
+        return other.type == "water";
+
     }
 
     update(row, col) {
-        // TODO update sand
+        // Fall due to gravity
+        let newRow = row + 1;
+    
+        // If nothing below move down
+        if (!moveParticle(row, col, newRow, col)) {
+            // Try to move left
+            if (!moveParticle(row, col, newRow, col-1, this.swap)) {
+                
+                moveParticle(row, col, newRow, col+1, this.swap)
+            }
+        }
     }
 }
+
 
 /**
  * Create particle based on dropdown name
@@ -62,6 +75,195 @@ export class Sand extends Particle {
 export function checkParticleType(value) {
     if (value == "Sand") {
         return new Sand();
-    } 
-    // TODO create new particles
+    } else if (value == "Water") {
+        return new Water();
+    } else if (value == "Ice") {
+        return new Ice();
+    } else if (value == "Fire") {
+        return new Fire();
+    } else if (value == "Wood") {
+        return new Wood();
+    } else if (value == "Stone") {
+        return new Stone();
+    } else if (value == "Dirt") {   
+        return new Dirt();
+    }
+    return null;
+}
+/**
+ * Wood particle
+ */
+export class Wood extends Particle {
+    constructor() {
+        super(); // Call the constructor of the Particle class
+        this.color = "brown";
+        this.type = "wood";
+    }
+
+    update(row, col) {
+        // Check all adjacent cells for water and grow wood in empty spaces
+        const directions = [
+            [1, 0],  // Down
+            [-1, 0], // Up
+            [0, 1],  // Right
+            [0, -1], // Left
+        ];
+
+        for (const [dRow, dCol] of directions) {
+            const neighbor = getParticle(row + dRow, col + dCol);
+
+            // If water is adjacent and the space is empty, grow wood
+            if (neighbor?.type === "water") {
+                const growDirections = directions.filter(([gRow, gCol]) => {
+                    const growNeighbor = getParticle(row + gRow, col + gCol);
+                    return !growNeighbor; // Check for empty space
+                });
+
+                for (const [gRow, gCol] of growDirections) {
+                    setParticle(row + gRow, col + gCol, new Wood());
+                }
+            }
+        }
+    }
+}
+/**
+ * Fire particle
+ */
+export class Fire extends Particle {
+    constructor() {
+        super(); // Call the constructor of the Particle class
+        this.color = "red";
+        this.type = "fire";
+    }
+
+    update(row, col) {
+        // Check all adjacent cells for wood and remove it
+        const directions = [
+            [1, 0],  // Down
+            [-1, 0], // Up
+            [0, 1],  // Right
+            [0, -1], // Left
+        ];
+
+        for (const [dRow, dCol] of directions) {
+            const neighbor = getParticle(row + dRow, col + dCol);
+
+            // If wood is adjacent, remove it
+            if (neighbor?.type === "wood") {
+                setParticle(row + dRow, col + dCol, null); // Remove wood
+            }
+        }
+    }
+}
+/**
+ * Ice particle
+ */
+export class Ice extends Particle {
+    constructor() {
+        super(); // Call the constructor of the Particle class
+        this.color = "lightblue";
+        this.type = "ice";
+    }
+
+    update(row, col) {
+        // Check all adjacent cells for water and convert them to ice
+        const directions = [
+            [1, 0],  // Down
+            [-1, 0], // Up
+            [0, 1],  // Right
+            [0, -1], // Left
+        ];
+
+        for (const [dRow, dCol] of directions) {
+            const neighbor = getParticle(row + dRow, col + dCol);
+            if (neighbor?.type === "water") {
+                setParticle(row + dRow, col + dCol, new Ice());
+            }
+        }
+    }
+}
+/**
+ * Rock particle
+ */
+export class Stone extends Particle {
+    constructor() {
+        super(); // Call the constructor of the Particle class
+        this.color = "gray";
+        this.type = "stone";
+    }
+    // No update or swap needed!
+}
+
+
+/**
+ * Dirt particle
+ */
+export class Dirt extends Sand {
+    constructor() {
+        super(); // Call the constructor of the Sand class
+        this.color = "brown";
+        this.type = "dirt";
+    }
+    // No update or swap needed! It inherits from Sand.
+}
+
+export class Water extends Particle {
+    constructor() {
+        super();
+        this.color = "blue";
+        this.type = "water";
+    }
+
+    update(row, col) {
+        // Make water turn dirt into grass when it touches it
+        if (getParticle(row + 1, col)?.type == "dirt") {
+            // Remove water and change dirt to grass
+            setParticle(row + 1, col, new Grass());
+            setParticle(row, col, null);
+            return;
+        }
+
+        // Remove fire when touched by water
+        const directions = [
+            [1, 0],  // Down
+            [-1, 0], // Up
+            [0, 1],  // Right
+            [0, -1], // Left
+        ];
+
+        for (const [dRow, dCol] of directions) {
+            const neighbor = getParticle(row + dRow, col + dCol);
+            if (neighbor?.type === "fire") {
+                setParticle(row + dRow, col + dCol, null); // Remove fire
+                setParticle(row, col, null); // Remove water
+                return;
+            }
+        }
+
+        // If sand is dropped on water, let it sink
+        if (getParticle(row + 1, col) && getParticle(row + 1, col).type == "sand") {
+            moveParticle(row, col, row + 1, col, super.swap);
+        }
+
+        // Try to move down
+        if (getRandomInt(0, 2) && !getParticle(row + 1, col)) {
+            moveParticle(row, col, row + 1, col, super.swap);
+        }
+
+        // Move left or right
+        if (getRandomInt(0, 1) && !getParticle(row, col + 1)) {
+            moveParticle(row, col, row, col + 1, super.swap);
+        }
+        // Move diagonally left
+        else if (getRandomInt(0, 1) && !getParticle(row + 1, col - 1)) {
+            moveParticle(row, col, row + 1, col - 1, super.swap);
+        }
+        // Move diagonally right
+        else if (getRandomInt(0, 1) && !getParticle(row + 1, col + 1)) {
+            moveParticle(row, col, row + 1, col + 1, super.swap);
+        }
+        else if (!getParticle(row, col - 1)) {
+            moveParticle(row, col, row, col - 1, super.swap);
+        }
+    }
 }
